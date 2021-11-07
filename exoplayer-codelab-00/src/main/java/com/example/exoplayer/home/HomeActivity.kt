d3f2c3dae8aa7d9dc25f6ef65a14084405c22316
alif <alif.sip@gmail.com>
2021-11-07 14:31:14 +0700

    create PlayerFragment.kt, move functionalities to PlayerFragment.kt

--- exoplayer-codelab-00/src/main/java/com/example/exoplayer/home/HomeActivity.kt
+++ exoplayer-codelab-00/src/main/java/com/example/exoplayer/home/HomeActivity.kt
@@ -1,26 +1,13 @@
 package com.example.exoplayer.home
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.MotionEvent
-import android.view.View
-import android.view.ViewConfiguration
 import androidx.appcompat.app.AppCompatActivity
-import com.example.exoplayer.R
 import com.example.exoplayer.databinding.ActivityHomeBinding
 import com.example.exoplayer.domain.Movie
 import com.example.exoplayer.domain.MovieSection
-import com.google.android.exoplayer2.MediaItem
-import com.google.android.exoplayer2.SimpleExoPlayer
-import com.google.android.exoplayer2.util.MimeTypes
-import com.google.android.exoplayer2.util.Util
 import java.util.*
-import kotlin.math.abs
-import kotlin.math.absoluteValue
 
 class HomeActivity : AppCompatActivity(), HomeListAdapter.Companion.Interaction {
-
-    private var player: SimpleExoPlayer? = null
 
     private val viewBinding by lazy(LazyThreadSafetyMode.SYNCHRONIZED) {
         ActivityHomeBinding.inflate(layoutInflater)
@@ -37,75 +24,6 @@ class HomeActivity : AppCompatActivity(), HomeListAdapter.Companion.Interaction 
         setContentView(viewBinding.root)
         setUpHomeList()
         setUpToolbar()
-        setPlayerSwipeToMinimizeListener()
-        setClickListeners()
-        initPlayer()
-    }
-
-    private fun initPlayer() {
-        player = SimpleExoPlayer.Builder(this)
-            .build()
-            .also {
-                viewBinding.playerView.player = player
-
-                val mediaItem = MediaItem.Builder()
-                    .setUri(getString(R.string.media_url_dash))
-                    .setMimeType(MimeTypes.APPLICATION_MPD)
-                    .build()
-
-                it.setMediaItem(mediaItem)
-            }
-
-        player?.apply {
-            prepare()
-        }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        if (Util.SDK_INT >= 24) {
-            initPlayer()
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        hideSystemUi()
-        if (Util.SDK_INT < 24 || player == null) {
-            initPlayer()
-        }
-    }
-
-    @SuppressLint("InlinedApi")
-    private fun hideSystemUi() {
-        viewBinding.playerView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE
-                or View.SYSTEM_UI_FLAG_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        if (Util.SDK_INT < 24) {
-            releasePlayer()
-        }
-    }
-
-    override fun onStop() {
-        super.onStop()
-        if (Util.SDK_INT >= 24) {
-            releasePlayer()
-        }
-    }
-
-    private fun releasePlayer() {
-        player?.run {
-            release()
-        }
-
-        player = null
     }
 
     private fun setUpToolbar() {
@@ -114,12 +32,6 @@ class HomeActivity : AppCompatActivity(), HomeListAdapter.Companion.Interaction 
             setDisplayShowTitleEnabled(false)
             setDisplayShowHomeEnabled(false)
             setDisplayHomeAsUpEnabled(false)
-        }
-    }
-
-    private fun setClickListeners() {
-        viewBinding.playerSectionView.setOnClickListener {
-            animateExpandPlayerSection()
         }
     }
 
@@ -192,60 +104,24 @@ class HomeActivity : AppCompatActivity(), HomeListAdapter.Companion.Interaction 
     }
 
     override fun onMovieClicked() {
-        viewBinding.root.transitionToEnd()
+//        viewBinding.root.transitionToEnd()
     }
 
-    private fun setPlayerSwipeToMinimizeListener() {
-        viewBinding.playerView.setOnTouchListener(object : View.OnTouchListener {
-            var startY = 0F
-            var startX = 0F
-            val touchSlop = ViewConfiguration.get(this@HomeActivity).scaledTouchSlop
-
-            override fun onTouch(v: View?, event: MotionEvent): Boolean {
-                when (event.action) {
-                    MotionEvent.ACTION_DOWN -> {
-                        startY = event.y
-                        startX = event.x
-
-                        return true
-                    }
-                    MotionEvent.ACTION_UP -> {
-                        val dy = event.y - startY
-
-                        if (isAClick(startX, event.x, startY, event.y)) {
-                            animateExpandPlayerSection()
-                        } else if (dy.absoluteValue > touchSlop) {
-                            animateMinimizePlayerSection()
-                        }
-                    }
-                }
-
-                return false
-            }
-
-            private fun isAClick(startX: Float, endX: Float, startY: Float, endY: Float): Boolean {
-                val differenceX = abs(startX - endX)
-                val differenceY = abs(startY - endY)
-                return (differenceX > touchSlop || differenceY > touchSlop).not()
-            }
-        })
-    }
-
-    private fun animateMinimizePlayerSection() {
-        viewBinding.motionLayout.apply {
-            if (currentState == R.id.minimized) return@apply
-
-            setTransition(R.id.expanded, R.id.minimized)
-            transitionToState(R.id.minimized)
-        }
-    }
-
-    private fun animateExpandPlayerSection() {
-        viewBinding.motionLayout.apply {
-            if (currentState == R.id.expanded) return@apply
-
-            setTransition(R.id.minimized, R.id.expanded)
-            transitionToState(R.id.expanded)
-        }
-    }
+//    private fun animateMinimizePlayerSection() {
+//        viewBinding.motionLayout.apply {
+//            if (currentState == R.id.minimized) return@apply
+//
+//            setTransition(R.id.expanded, R.id.minimized)
+//            transitionToState(R.id.minimized)
+//        }
+//    }
+//
+//    private fun animateExpandPlayerSection() {
+//        viewBinding.motionLayout.apply {
+//            if (currentState == R.id.expanded) return@apply
+//
+//            setTransition(R.id.minimized, R.id.expanded)
+//            transitionToState(R.id.expanded)
+//        }
+//    }
 }
---
+++ exoplayer-codelab-00/src/main/java/com/example/exoplayer/player/PlayerFragment.kt
@@ -0,0 +1,113 @@
+package com.example.exoplayer.player
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.exoplayer.R
+import com.example.exoplayer.databinding.FragmentPlayerBinding
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.util.MimeTypes
+import com.google.android.exoplayer2.util.Util
+
+internal class PlayerFragment : Fragment() {
+
+    private var player: SimpleExoPlayer? = null
+    private var _binding: FragmentPlayerBinding? = null
+    private val binding: FragmentPlayerBinding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentPlayerBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initPlayer()
+    }
+
+    private fun initPlayer() {
+        player = SimpleExoPlayer.Builder(requireContext())
+            .build()
+            .also {
+                binding.playerView.player = player
+
+                val mediaItem = MediaItem.Builder()
+                    .setUri(getString(R.string.media_url_dash))
+                    .setMimeType(MimeTypes.APPLICATION_MPD)
+                    .build()
+
+                it.setMediaItem(mediaItem)
+            }
+
+        player?.apply {
+            prepare()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT >= 24) {
+            initPlayer()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        hideSystemUi()
+        if (Util.SDK_INT < 24 || player == null) {
+            initPlayer()
+        }
+    }
+
+    @SuppressLint("InlinedApi")
+    private fun hideSystemUi() {
+        binding.playerView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT < 24) {
+            releasePlayer()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT >= 24) {
+            releasePlayer()
+        }
+    }
+
+    private fun releasePlayer() {
+        player?.run {
+            release()
+        }
+
+        player = null
+    }
+
+    //    private fun setClickListeners() {
+//        viewBinding.playerSectionView.setOnClickListener {
+//            animateExpandPlayerSection()
+//        }
+//    }
+}
